c398bbe65c47755a6b38922db2b4fdaef2af1fdf
ritika0211 <56756094+ritika0211@users.noreply.github.com>
2024-04-29 09:38:15 +0000

    adding all three files

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 2345
+
+let accountEmail = "ritika@git.com"
+
+var accountPass = "345"
+
+accountCity ="delhi"
+
+// accountId ="89"
+
+accountEmail = "edrf@git.com"
+accountPass ="3456"
+accountCity = "evfhejdf"
+let accountState;
+console.log (accountId)
+console.table ([accountId, accountEmail, accountPass, accountCity, accountState])
+
+
+/*
+We need not to prefer var as it will behave diffrently in block scope and functional scope.
+We have to use let and const.
+*/
---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,86 @@
+/*
+let score = "33abc"
+
+console.log(typeof (score))
+console.log(typeof score)
+
+let ValueInNumber = Number(score)
+console.log(typeof ValueInNumber)
+
+console.log(ValueInNumber);
+
+*/
+// for null value the valueInNumber is 0
+/*
+let score = null
+
+console.log(typeof (score))
+console.log(typeof score)
+
+let ValueInNumber = Number(score)
+console.log(typeof ValueInNumber)
+
+console.log(ValueInNumber);
+
+*/
+// for undefined value the valueInNumber is NaN
+/*
+let score = undefined
+
+console.log(typeof (score))
+console.log(typeof score)
+
+let ValueInNumber = Number(score)
+console.log(typeof ValueInNumber)
+
+console.log(ValueInNumber);
+*/
+
+
+// for true/false value the valueInNumber is 1/0
+/*
+let score = true
+
+console.log(typeof (score))
+console.log(typeof score)
+
+let ValueInNumber = Number(score)
+console.log(typeof ValueInNumber)
+
+console.log(ValueInNumber);
+*/
+
+// for string value the valueInNumber is NaN
+/*
+let score = "ritika"
+
+console.log(typeof (score))
+console.log(typeof score)
+
+let ValueInNumber = Number(score)
+console.log(typeof ValueInNumber)
+
+console.log(ValueInNumber);
+*/
+
+
+// lets convert a number to bollean
+
+let isLoggedIn = ""
+
+let valueInboolean = Boolean(isLoggedIn)
+
+console.log(valueInboolean)
+
+
+// 1=> true
+// 0=> false
+// 23 => true
+// "ritika" => true
+// "" => false
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
